cb0627fb03cf77dccf9c3ff1671013798b88b143
Rasel3925 <49741825+Rasel3925@users.noreply.github.com>
2019-04-25 22:09:31 -0400

    initial commit

---
+++ Homeworkcae/src/hondaCare/hondacarsuv.java
@@ -0,0 +1,16 @@
+package hondaCare;
+
+public class hondacarsuv {
+public static void main(String[]args) { 
+	boolean billionear= true;
+	if (billionare) {
+		
+		
+	string suv1 ="private jat";
+    string suv2="mercedezBenz";
+    
+    system.out.println(suv1=suv2);}else{
+    	string suv3=" honda";
+    }
+	}
+}
