73e15d631c8758bdf1a6082d38fdd52bcfc90fdf
Rasel3925 <49741825+Rasel3925@users.noreply.github.com>
2019-04-26 03:57:06 -0400

    Commit java

---
+++ operatorExpression/src/operatorExpression/Calculator.java
@@ -0,0 +1,20 @@
+package operatorExpression;
+
+public class Calculator {
+
+	public static void main(String[]args) { 
+		
+		int num1, num2, result;
+		num1=20;
+		num2=24;
+		result =num1 + num2;
+		System.out.println("sum = "+result);
+		
+		result =num1 - num2;
+		System.out.println("sub = "+result);
+		
+		result =num1 / num2;
+		System.out.println("Div= "+result);
+		
+	}
+}
